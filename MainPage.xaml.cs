46924c92c39f3c22d200b23c61bf772b137c5187
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2024-08-23 22:21:59 -0300

    Porcentagem funciona!

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,18 +10,25 @@
 
         private void SetLowTip_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
         private void SetHighTip_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
         private void RoundUpButton_Clicked(object sender, EventArgs e)
         {
-
+            
         }
         private void RoundDownButton_Clicked(object sender, EventArgs e)
         {
+
+        }
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
 
         }
     }
