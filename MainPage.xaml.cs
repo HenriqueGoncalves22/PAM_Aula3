ff30a3549ba28c3b76817084f84d248d3a4ba60c
Henrique Gonçalves <henrique.gonc.gamer@gmail.com>
2024-08-23 21:51:38 -0300

    Atualiza

---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace TipCaculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void SetHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TipCaculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
